f6922cf2f59518cb04a9df779466f36344e80ac6
MattG5 <rabEnStock@yahoo.fr>
2024-01-22 22:24:38 +0100

    Squashed commit of the following:
    commit 631daac6dfbc2c4cf157140105dd6e0dd2a8ea65
    Author: MattG5 <rabEnStock@yahoo.fr>
    Date:   Mon Jan 22 22:19:50 2024 +0100
    
        add the parametrized url for hello as example
    
    commit b6f6eef9e3ec4394779f2cf813fa6127a51d46e2
    Author: MattG5 <rabEnStock@yahoo.fr>
    Date:   Mon Jan 22 22:13:15 2024 +0100
    
        Use upgraded calculatrice which keep values and operator and use login caption in button to login
    
    commit affe517917cee3e750f0dfa52b0d5d7ef5e5814a
    Author: Fusion <dante1602@gmail.com>
    Date:   Wed Nov 22 19:16:46 2023 +0100
    
        Requirements
    
    commit 2c00c2c11f7a425b48bfdbde059546a723175cc0
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Mon Oct 16 17:28:20 2023 +0200
    
        ajout authentification très peu restrictive
    
    commit 149e78a793f22fc2f73acc364c83bd31e35784f8
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Mon Oct 16 16:37:47 2023 +0200
    
        ajout calculatrice
    
    commit 3b6915ec521be146d1f69e68992d1e9d7513217a
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Wed Oct 11 09:26:43 2023 +0200
    
        ajout jinja et css pour render template
    
    commit 1539f4cea000e837803c5b9c4dbac874c8824074
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Wed Oct 11 09:03:12 2023 +0200
    
        ajout decommentaires
    
    commit 204b72bc77f8d3aee3854493bce7bc65ff56aa81
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Wed Oct 11 08:59:39 2023 +0200
    
        simplification à l'extreme de l'application
    
    commit 608a3f3d66e3db3f595f36c977b09ab800b87d19
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Tue Oct 10 19:06:57 2023 +0200
    
        ajout installation md
    
    commit 1f93daa01dd68571c95685e561fd5598d4f0fbcf
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Mon Oct 9 10:23:26 2023 +0200
    
        ajout d'une base html, css, jinja
    
    commit da35c0fdae2fe5302dff2d2b2880d70f979d169a
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Mon Oct 9 09:06:17 2023 +0200
    
        application de hello wolrd basic

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # On importe le module Flask 
 
-# /!\ On ajoute à l'import de flask le module requests
-from flask import Flask, render_template,request
+# /!\ On ajoute à l'import de flask le module url_for, redirect et session
+from flask import Flask, render_template,request,url_for, redirect,session
 
 # On crée une instance d'application
 app = Flask(__name__)
@@ -10,7 +10,78 @@ app = Flask(__name__)
 @app.route('/')
 def bonjour():
     # Le retour de la route dans le navigateur sera Bonjour
-    return "Bonjour"
+    return helloLang('fr')
+
+# On définit une route par défaut
+@app.route('/hello/<string:lang>')
+def helloLang(lang):
+    # Le retour de la route dans le navigateur sera Bonjour
+    txt = ""
+    if lang == 'fr':
+        txt = "Bonjour"
+    elif lang == 'en':
+        txt = "Hello"
+    elif lang == 'de':
+        txt = "Guten Tag"
+    return txt
+
+
+######################
+## AUTHENTIFICATION ##
+######################
+
+# On configure une clé secrete pour sécurisé l'application
+app.config['SECRET_KEY'] = 'CleSecreteDeMalade!'
+
+# On crée une route pour l'authentification
+@app.route('/login', methods=['GET','POST'])
+def login():
+    # Si la méthode d'accès à la page est un post
+    if request.method == 'POST':
+        # On crée la session avec le nom utilisateur envoyé par l'utilisateur
+        session['user'] = request.form['username']
+        # Et on envoi à son tableau de bord
+        return redirect(url_for('dashboard'))
+    # Si la méthode d'accès à la page est un get on envoi la page html de login.
+    return render_template('login.html')
+
+# On crée une route pour le dashboard
+@app.route('/dashboard')
+def dashboard():
+    # Si il y a un utilisateur connecté on lui présente soin dashboard
+    if 'user' in session:
+        #La connexion est autorisé
+        message = f"Bienvenue dans votre espace personnelle {session['user']}!"
+        return message
+    # Autrement on le renvoi à la page de login
+    return redirect(url_for('login'))
+
+# On se fait une petite page de logout parce qu'on veux pouvoir se déconnecter.
+@app.route('/logout')
+def logout():
+    # Si il y avait un utilisateur connecté
+    if 'user' in session:
+        # On le déconnecte
+        session.pop('user',None)
+        # On l'informe qu'il a bien été déconnecté
+        return "Vous avez bien été déconnecté"
+    # Autrement on se fou de lui.
+    return "Vous n'etiez pas connecté, comment voulez vous que je vous déconnecte?"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # On définit une route pour afficher le résultat d'un template html
 @app.route('/get_render')
@@ -27,40 +98,56 @@ def get_jinja():
     return render_template('get_jinja.html', ma_list = la_liste)
 
 # On définit une route pour afficher un calculatrice
-@app.route('/calculatrice', methods=['GET'])
-def calculatriceGet():
-    return render_template('calculatrice.html', result='')
+@app.route('/calculatrice', methods=['GET', 'POST'])
+def calculatrice():
+    if 'user' in session:
+        # Le résultat par défault sera none 
+        result = ""
+        # Si la méthod est un post c'est que le formulaire a envoyé une demande
+        if request.method == 'POST':
+            # On récupère la valeur 1 depuis la requete envoyé
+            num1 = float(request.form['num1'])
+            # On récupère la valeur 2 depuis la requete envoyé
+            num2 = float(request.form['num2'])
+            # On récupère l'opérateur' depuis la requete envoyé
+            operator = request.form['operator']
+            # Si l'opérateur est ajout
+            if operator == 'add':
+                # Le résulatat renvoyé sera
+                result = f"Résultat : {num1 + num2}"
+            # Si l'opérateur est une soustration    
+            elif operator == 'subtract':
+                result = f" Résultat : {num1 - num2}"
+            # Si l'opérateur est une multiplication
+            elif operator == 'multiply':
+                result = f"Résultat : {num1 * num2}"
+            # Si l'opérateur est une division
+            elif operator == 'divide':
+                # Les division par zero sont impossible!!!
+                if num2 != 0:
+                    result = f"Résultat : {num1 / num2}"
+                else:
+                    result = "Division par zéro impossible"
+            # Puis on renvoie la page de calcul
+            return render_template('calculatrice.html', result=result, num1Value=num1, ope=operator, num2Value=num2,utilisateur=session['user'])
+        elif request.method == 'GET':
+            # on renvoie la page de calcul
+            return render_template('calculatrice.html', result=result,utilisateur=session['user'])
+    else : 
+        return redirect(url_for('login'))
 
-@app.route('/calculatrice', methods=['POST'])
-def calculatricePost():
-    # Le résultat par défault sera none 
-    result = ""
-    # Si la méthod est un post c'est que le formulaire a envoyé une demande
-    # On récupère la valeur 1 depuis la requete envoyé
-    num1 = float(request.form['num1'])
-    # On récupère la valeur 2 depuis la requete envoyé
-    num2 = float(request.form['num2'])
-    # On récupère l'opérateur' depuis la requete envoyé
-    operator = request.form['operator']
-    # Si l'opérateur est ajout
-    if operator == 'add':
-        # Le résulatat renvoyé sera
-        result = f"Résultat : {num1 + num2}"
-    # Si l'opérateur est une soustration    
-    elif operator == 'subtract':
-        result = f" Résultat : {num1 - num2}"
-    # Si l'opérateur est une multiplication
-    elif operator == 'multiply':
-        result = f"Résultat : {num1 * num2}"
-    # Si l'opérateur est une division
-    elif operator == 'divide':
-        # Les division par zero sont impossible!!!
-        if num2 != 0:
-            result = f"Résultat : {num1 / num2}"
-        else:
-            result = "Division par zéro impossible"
-    # Dans tous les cas on renvoi la page de calcul
-    return render_template('calculatrice.html', result=result, num1Value=num1, ope=operator, num2Value=num2)
+
+
+
+
+
+
+
+
+
+
+
+
 
 #  Exécutez l'application si ce script est exécuté en tant que programme principal
 if __name__ == '__main__':
