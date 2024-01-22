a0ba996ed718356fc0570022123693c614154a62
MattG5 <rabEnStock@yahoo.fr>
2024-01-22 21:47:25 +0100

    Squashed commit of the following:
    commit 7f99fb980243de5abdff46b19ed68721b4cf8725
    Author: MattG5 <rabEnStock@yahoo.fr>
    Date:   Fri Dec 22 16:07:50 2023 +0100
    
        retire les options jinja du renderer classique (lien vers le css)
    
    commit a53d5761385d18f6ac54ccdcf0ba4b3b6facdc05
    Author: FusionNucleaire <63256987+FusionNucleaire@users.noreply.github.com>
    Date:   Sat Oct 14 11:31:22 2023 +0200
    
        Add files via upload
    
    commit 3b6915ec521be146d1f69e68992d1e9d7513217a
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Wed Oct 11 09:26:43 2023 +0200
    
        ajout jinja et css pour render template
    
    commit 1539f4cea000e837803c5b9c4dbac874c8824074
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Wed Oct 11 09:03:12 2023 +0200
    
        ajout decommentaires
    
    commit 204b72bc77f8d3aee3854493bce7bc65ff56aa81
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Wed Oct 11 08:59:39 2023 +0200
    
        simplification à l'extreme de l'application
    
    commit 608a3f3d66e3db3f595f36c977b09ab800b87d19
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Tue Oct 10 19:06:57 2023 +0200
    
        ajout installation md
    
    commit 1f93daa01dd68571c95685e561fd5598d4f0fbcf
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Mon Oct 9 10:23:26 2023 +0200
    
        ajout d'une base html, css, jinja
    
    commit da35c0fdae2fe5302dff2d2b2880d70f979d169a
    Author: SOUFFRAN Armel <asouffran-reseau@tibco.fr>
    Date:   Mon Oct 9 09:06:17 2023 +0200
    
        application de hello wolrd basic

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-# On importe le module Flask
-from flask import Flask
+# On importe le module Flask 
+
+# /!\ On ajoute à l'import de flask le module render template
+from flask import Flask, render_template
 
 # On crée une instance d'application
 app = Flask(__name__)
@@ -10,6 +12,17 @@ def bonjour():
     # Le retour de la route dans le navigateur sera Bonjour
     return "Bonjour"
 
+# On définit une route pour afficher le résultat d'un template html
+@app.route('/get_render')
+def get_render():
+# Les templates sont dans le dossier templates
+    return render_template('get_render.html') # ATTENTION il faut redémarré l'application pour modifier le contenu du html
+
+@app.route('/get_jinja')
+def get_jinja():
+
+    la_liste = ['Salut','Comment tu va?', 'bienvenue dans le monde magique de jinja!']
+    return render_template('get_jinja.html', ma_list = la_liste)
 
 #  Exécutez l'application si ce script est exécuté en tant que programme principal
 if __name__ == '__main__':
